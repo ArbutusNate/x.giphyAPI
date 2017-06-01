7abe2a7f5b83f4cc586f934d4476ae6e608c0588
ArbutusNate <arbutusnate@gmail.com>
2017-06-01 14:34:19 -0700

    styling updated

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,4 +1,4 @@
-var btnarray = ["30 Rock", "Comedy Bang! Bang!", "Community", "It's Always Sunny", "Parks and Rec"];
+var btnarray = ["30 Rock", "Comedy Bang! Bang!", "Community", "Always Sunny", "Parks and Rec"];
 function addbuttons(){
     $(".buttonzone").empty();
     for (var i = 0; i < btnarray.length; i++) {
